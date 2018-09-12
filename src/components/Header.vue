22f8b21944b65da473159b8532d0d99f6fed72b3
Volodymyr Ivzhenko <ivzhen4@gmail.com>
2018-09-12 15:33:09 +0300

    included uikit & create base route

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active">
+                    <router-link to="/" class="uk-active">Home</router-link>
+                </li>
+                <li>
+                    <router-link to="/hello">Hello page</router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
 
 Vue.config.productionTip = false
+UIkit.use(Icons)
 
 /* eslint-disable no-new */
 new Vue({
